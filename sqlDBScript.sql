873fa17979f7a6b8734d84c85fe9656bb96f0ff0
Arcedo <arcedo.marc@gmail.com>
2024-02-05 09:41:19 +0100

    [ADD] MySQL create database for fundflow

---
+++ sqlDBScript.sql
@@ -0,0 +1,85 @@
+DROP DATABASE IF EXISTS fundflow;
+CREATE DATABASE fundflow;
+
+/*CREATE USER 'fundflow'@'%' IDENTIFIED BY 'y0uNever$ee4CumM4n';
+GRANT ALL PRIVILEGES ON fundflow.* TO 'fundflow'@'%';
+FLUSH PRIVILEGES;*/
+
+USE fundflow;
+
+CREATE TABLE users (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    userRole VARCHAR(30) NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    name VARCHAR(50),
+    lastName VARCHAR(50),
+    biography VARCHAR(250),
+    verified BOOLEAN DEFAULT FALSE,
+    profilePictureSrc VARCHAR(250),
+    bannerPictureSrc VARCHAR(250),
+    registerDate DATE
+);
+
+CREATE TABLE followsUsers (
+    idFollowingUser BIGINT UNSIGNED NOT NULL,
+    idFollowedUser BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (idFollowingUser, idFollowedUser),
+    FOREIGN KEY (idFollowingUser) REFERENCES users(id),
+    FOREIGN KEY (idFollowedUser) REFERENCES users(id)
+);
+
+CREATE TABLE categories (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE projects (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    idCategory BIGINT UNSIGNED NOT NULL,
+    idUser BIGINT UNSIGNED NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    description VARCHAR(200) NOT NULL,
+    priceGoal BIGINT UNSIGNED NOT NULL,
+    collGoal BIGINT UNSIGNED NOT NULL,
+    creationDate DATE,
+    deadlineDate DATE,
+    views BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (idCategory) REFERENCES categories(id),
+    FOREIGN KEY (idUser) REFERENCES users(id)
+);
+
+CREATE TABLE sponsors (
+    idProject BIGINT UNSIGNED NOT NULL,
+    idUser BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (idProject, idUser),
+    FOREIGN KEY (idProject) REFERENCES projects(id),
+    FOREIGN KEY (idUser) REFERENCES users(id)
+);
+
+CREATE TABLE collaborators (
+    idProject BIGINT UNSIGNED NOT NULL,
+    idUser BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (idProject, idUser),
+    FOREIGN KEY (idProject) REFERENCES projects(id),
+    FOREIGN KEY (idUser) REFERENCES users(id)
+);
+
+CREATE TABLE blockedUsers (
+    id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
+    idUser BIGINT UNSIGNED NOT NULL,
+    blockDateStart DATE,
+    blockDateFinish DATE,
+    FOREIGN KEY (idUser) REFERENCES users(id)
+);
+
+CREATE TABLE projectBlogs (
+    id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
+    idProject BIGINT UNSIGNED NOT NULL,
+    title VARCHAR(50),
+    subtitle VARCHAR(50),
+    body VARCHAR(250),
+    FOREIGN KEY (idProject) REFERENCES projects(id)
+);
+
+
