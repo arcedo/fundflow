5827c318b3ac5063c340f6bf89c95e83b9db31a7
Arcedo <arcedo.marc@gmail.com>
2024-04-30 10:49:08 +0200

    [ADD] hardcoded values in db

--- scripts/script.sql
+++ scripts/script.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS fundflow;
 CREATE DATABASE fundflow;
 
--- CREATE USER 'fundflow'@'%' IDENTIFIED BY 'y0uNeversee4CumM4n';
+-- CREATE USER 'fundflow'@'%' IDENTIFIED BY 'fundflow2024';
 -- GRANT ALL PRIVILEGES ON fundflow.* TO 'fundflow'@'%';
 -- FLUSH PRIVILEGES;
 
@@ -70,38 +70,12 @@ INSERT INTO fundflow.categories (name) VALUES ('Art'), ('Music'), ('Books'), ('G
 
 -- Admin User
 -- If this user is not created some tests in the backend will fail
-INSERT INTO fundflow.users (`role`, username, email, hashPassword, verified, `url`)
-VALUES (true, 'admin', 'arcedo.marc@gmail.com', '$2b$10$uqYGJ4JB/ijaFZWCYePMrOH8ZwMGrUTuIATE09/Lwn7648Sod4u7K', true, 'admin');
+INSERT INTO fundflow.users (`role`, username, email, hashPassword, verifiedEmail, `url`)
+VALUES (true, 'admin', 'marcarcedoalvaro25@gmail.com', '$2b$10$uqYGJ4JB/ijaFZWCYePMrOH8ZwMGrUTuIATE09/Lwn7648Sod4u7K', true, 'admin');
 
--- CREATE TABLE sponsors (
---    idProject BIGINT UNSIGNED NOT NULL,
---    idUser BIGINT UNSIGNED NOT NULL,
---    PRIMARY KEY (idProject, idUser),
---    FOREIGN KEY (idProject) REFERENCES projects(id),
---    FOREIGN KEY (idUser) REFERENCES users(id)
--- );
+INSERT INTO fundflow.users (username, email, hashPassword, `url`) VALUES ('arcedo', 'arcedo.marc@mail.com', '$2b$10$mEGm4Mxi1ryqKOeN0GVWxu9phXqOaUrLQ7b60XJR4HYfXSCNGs4wG', 'arcedo');
+INSERT INTO fundflow.users (username, email, hashPassword, `url`) VALUES ('enric', 'armengolgarciaenric@mail.com', '$2b$10$wT99AhzxWKkX3ZT6tlRo6Oiw5pKjV8wM92G7ywLtZ2b/QhhKBpFPS', 'enric');
 
--- CREATE TABLE collaborators (
---     idProject BIGINT UNSIGNED NOT NULL,
---     idUser BIGINT UNSIGNED NOT NULL,
---     PRIMARY KEY (idProject, idUser),
---     FOREIGN KEY (idProject) REFERENCES projects(id),
---     FOREIGN KEY (idUser) REFERENCES users(id)
--- );
 
--- CREATE TABLE blockedUsers (
---     id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
---     idUser BIGINT UNSIGNED NOT NULL,
---     blockDateStart DATE,
---     blockDateFinish DATE,
---     FOREIGN KEY (idUser) REFERENCES users(id)
--- );
-
--- CREATE TABLE projectBlogs (
---     id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
---     idProject BIGINT UNSIGNED NOT NULL,
---     title VARCHAR(50),
---     subtitle VARCHAR(50),
---     body VARCHAR(250),
---     FOREIGN KEY (idProject) REFERENCES projects(id)
--- );
+INSERT INTO fundflow.projects (idCategory, idUser, title, description, url, priceGoal, currency, creationDate, deadlineDate, coverImageSrc) 
+VALUES (1, 2, 'Project One', 'Description of the project one', 'project-one', 10000, 'EUR', NOW(), '2025-02-01', 'project_1_cover.webp');
