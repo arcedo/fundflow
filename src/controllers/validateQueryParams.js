712e8c9d974e8e9ed04aea52e83cedc33348d028
Arcedo <arcedo.marc@gmail.com>
2024-05-21 14:11:44 +0200

    [FIX] limitation

--- src/controllers/validateQueryParams.js
+++ src/controllers/validateQueryParams.js
@@ -1,8 +1,8 @@
 function validateQueryParams(req, res, next) {
     const startIndex = parseInt(req.query.startIndex, 10);
     const limit = parseInt(req.query.limit, 10);
-    if (startIndex > 0 && limit > 0) {
-        return res.status(400).send({ message: 'startIndex and limit query parameters are required' });
+    if (isNaN(startIndex) || isNaN(limit) || startIndex < 0 || limit <= 0) {
+        return res.status(400).send({ message: 'startIndex and limit query parameters are required and must be valid numbers' });
     } else {
         // Store the validated values in the request object
         req.startIndex = startIndex;
