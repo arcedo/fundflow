d1eaf75c49d789dc61f5d50e85e857d4dc7d5da9
Arcedo <arcedo.marc@gmail.com>
2024-05-23 22:27:10 +0200

    [FIX] project reviews

--- src/routes/projectReviews.js
+++ src/routes/projectReviews.js
@@ -12,7 +12,7 @@ router.get('/:id/reviews', async (req, res) => {
         if (!reviews) {
             return res.status(404).send({ message: 'Reviews not found', code: 404 });
         }
-        res.status(200).send(reviews);
+        res.status(200).json(reviews);
     } catch (error) {
         console.error('Error in /:id/reviews route:', error);
         res.status(500).send({ message: 'Internal Server Error' });
@@ -25,7 +25,7 @@ router.get('/reviewing/byUser', verifyUserLogged, async (req, res) => {
         if (!reviews) {
             return res.status(404).send({ message: 'Reviews not found', code: 404 });
         }
-        res.status(200).send(reviews);
+        res.status(200).json(reviews);
     }
     catch (error) {
         console.error('Error in /byUser/reviewing route:', error);
@@ -39,7 +39,7 @@ router.get('/reviewed/byUser', verifyUserLogged, async (req, res) => {
         if (!reviews) {
             return res.status(404).send({ message: 'Reviews not found', code: 404 });
         }
-        res.status(200).send(reviews);
+        res.status(200).json(reviews);
     }
     catch (error) {
         console.error('Error in /byUser/reviewed route:', error);
@@ -64,7 +64,7 @@ router.post('/:id/reviews', verifyUserLogged, async (req, res) => {
             rating: rating,
         });
         await newReview.save();
-        res.status(201).send(newReview);
+        res.status(201).json(newReview);
     } catch (error) {
         console.error('Error in /:id/reviews route:', error);
         res.status(500).send({ message: 'Internal Server Error' });
@@ -75,16 +75,16 @@ router.delete('/:id/reviews/:idReview', verifyUserLogged, async (req, res) => {
     try {
         const review = await ReviewsProjects.findOne({ _id: req.params.idReview });
         if (!review) {
-            return res.status(404).json({ message: 'Review not found', code: 404 });
+            return res.status(404).send({ message: 'Review not found', code: 404 });
         }
         if (review.idUser !== req.userId) {
-            return res.status(403).json({ message: 'Forbidden', code: 403 });
+            return res.status(403).send({ message: 'Forbidden', code: 403 });
         }
         await ReviewsProjects.deleteOne({ _id: req.params.idReview });
         res.status(200).json({ message: 'Review deleted', code: 200 });
     } catch (error) {
         console.error('Error in DELETE /:id/reviews/:idReview route:', error);
-        res.status(500).json({ message: 'Internal Server Error', code: 500 });
+        res.status(500).send({ message: 'Internal Server Error', code: 500 });
     }
 });
 
